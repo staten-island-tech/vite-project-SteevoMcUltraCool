9032d751a75b7cd9ea99d584d22f3e6c6a35dbe0
SteevoMcUltraCool <stephen.radice23@sitechhs.com>
2022-11-03 14:03:38 -0400

    ee

--- vitework/main.js
+++ vitework/main.js
@@ -1,1 +1,24 @@
-console.log("Hi")
+const Theme = {
+  Toast: {
+    "--bgTopColor": "#f6e8d8",
+    "--bgMidColor": "#d09050",
+    "--bgBottomColor": "#782511",
+    "--barColor": "#e0b781",
+  },
+  Avocado: {
+    "--bgTopColor": "#f9f5b6",
+    "--bgMidColor": "#bad149",
+    "--bgBottomColor": "#123512",
+    "--barColor": "#702c18",
+  },
+};
+
+const DOM = {
+  root: document.documentElement,
+};
+
+function changeTheme(theme) {
+  theme.forEach((color, key) => {
+    DOM.root.style.setProperty(key, color);
+  });
+}

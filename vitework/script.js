979444e9d324b762f2f19633e7a8a3bfd9d80d46
SteevoMcCool <godzillakirby12@gmail.com>
2022-11-05 10:11:54 -0400

    jjoj

--- vitework/script.js
+++ vitework/script.js
@@ -2,20 +2,23 @@ const Theme = {
   Toast: {
     "--bgTopColor": "#f6e8d8",
     "--bgMidColor": "#d09050",
-    "--bgBottomColor": "#782511",
+    "--bgBottomColor": "#682511",
     "--barColor": "#e0b781",
+    "--barText":  "#332511"
   },
   Avocado: {
     "--bgTopColor": "#f9f5b6",
     "--bgMidColor": "#bad149",
     "--bgBottomColor": "#123512",
-    "--barColor": "#702c18",
+    "--barColor": "#602c18",
+    "--barText":  "#bfd74b"
   },
 };
 
 
 const DOM = {
   root: document.documentElement,
+  TDDM: document.getElementById("ThemeDropDownMenu")
 };
 
 function changeTheme(theme) {
@@ -24,4 +27,22 @@ function changeTheme(theme) {
  });
 }
 
+
+
+
 changeTheme(Theme.Avocado)
+
+
+function loadThemeDropDownMenu() {
+  Object.keys(Theme).forEach((key) => {
+    let newDIV = document.createElement("div")
+    newDIV.onclick = function(){changeTheme(Theme[key])}
+    newDIV.onmousedown = function(){newDIV.style.backgroundColor ="lightgray"}
+    newDIV.onmouseup = function(){newDIV.style.backgroundColor ="rgb(225,225,225)"}
+    newDIV.onmouseleave = function(){newDIV.style.backgroundColor ="rgb(225,225,225)"}
+
+    newDIV.innerHTML = key
+    DOM.TDDM.appendChild(newDIV)
+  } )
+}
+loadThemeDropDownMenu()
